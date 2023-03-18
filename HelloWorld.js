9f72af33cc918e35f2925e902eefa737496feb8f
Ratnesh Pasi <ratneshpasi03@gmail.com>
2023-03-19 02:26:06 +0530

    Same day practice

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,12 @@
 console.log("Hello world")
+
+// same file for practicing the variables
+
+num = 2+2
+console.log(num)
+console.log(num+3)
+
+// the concept of let 
+
+let userName = "Ratnesh"
+console.log(userName) 
