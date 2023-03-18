ae9e51e96a2a4fabea22e69eb6fe50a036d33b22
Ratnesh Pasi <ratneshpasi03@gmail.com>
2023-03-19 03:24:03 +0530

    some new concepts

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,12 +1,90 @@
 console.log("Hello world")
 
-// same file for practicing the variables
+// same file for practicing the variables -----------------
 
 num = 2+2
 console.log(num)
 console.log(num+3)
 
-// the concept of let 
+// the concept of let -----------------
 
 let userName = "Ratnesh"
 console.log(userName) 
+
+// constants concept -----------------
+
+let radius = 5
+const pi = 3.14
+let area
+
+radius=6 
+// pi=5 this can not happen as its defind as constant
+area = pi*radius*radius
+
+console.log(area)
+
+// concept of data types
+
+let data = 3               //number
+let user = "Ratnesh"       //string
+
+let num1= 23456789045675
+console.log(num1)
+
+//typeof function in java script
+
+console.log(typeof num1)
+console.log(typeof user)
+
+// hexa decimal number system im js 
+
+let num2 = 0xff
+console.log(num2)
+
+console.log(typeof num2)
+console.log(num2*5)
+
+// floating decimal number type in javascript
+
+let num3 = 1.23456789e34
+console.log(typeof num3)
+console.log(num3)
+
+//special way of writing zeros
+
+let num4 = 10_00_00_00_000
+console.log(num4)
+
+//infinty in js
+
+let a = 5/0
+let b = -5/0
+
+console.log(a)
+console.log(b)
+
+let c = Number.MAX_VALUE
+
+console.log(" c =" ,c)
+
+c = c*10
+
+console.log("10 * c =" ,c)
+
+let d = Number.MIN_VALUE
+
+console.log(" d =" ,d)
+
+d = d/10
+
+console.log("d / 10 =" ,d)
+
+// data type Big int in js
+
+let x = 5678998765345678n
+
+console.log("x = ",x)
+
+console.log("x * 5n = ", x*5n)
+
+console.log(typeof x)
