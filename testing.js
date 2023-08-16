b9dd5e064d7e46dae9430466315b5dc27a861433
Ratnesh Pasi <ratneshpasi03@gmail.com>
2023-08-16 11:29:55 +0530

    updates

--- testing.js
+++ testing.js
@@ -1,6 +1,19 @@
-var a = 5
-console.log("5" + +5)
-console.log(5 + +5)
-console.log("5" + +"5")
-console.log(7 + +"5" )
-console.log(+"5" )
+// var a = 5
+// console.log('"5" + +5 = ',"5" + +5)
+// console.log('5 + +5 = ',5 + +5)
+// console.log('"5" + +"5" = ',"5" + +"5")
+// console.log('5 + +"5" = ',5 + +"5" )
+// console.log(+"5" )
+
+// console.log('"5" + 5 = ',"5" + 5)
+// console.log('5 + 5 = ',5 + 5)
+// console.log('"5" + "5" = ',"5" + "5")
+// console.log('5 + "5" = ',5 + "5" )
+// console.log(+"5" )
+
+// ------------------------------
+
+// {
+//     console.log(b)
+//     // var b = 10 ;
+// }
